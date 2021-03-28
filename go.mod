c91c7cb6c27191151eb5c05014916dc65f43d4da
hakan turğay <turgayh@itu.edu.tr>
2021-03-28 23:35:17 +0300

    setup cors

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module todo-backend
 go 1.15
 
 require (
+	github.com/astaxie/beego v1.12.3
 	github.com/gin-gonic/gin v1.6.3
 	github.com/go-delve/delve v1.6.0 // indirect
 	github.com/go-playground/validator/v10 v10.4.1 // indirect
--- main.go
+++ main.go
@@ -8,6 +8,21 @@ import (
 	"github.com/joho/godotenv"
 )
 
+func CORS() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
 func main() {
 	err := godotenv.Load()
 	if err != nil {
@@ -16,6 +31,7 @@ func main() {
 
 	log.Print("Starting the grocery app")
 	r := gin.Default()
+	r.Use(CORS())
 	webapi.Route(r)
 	r.Run()
 }
