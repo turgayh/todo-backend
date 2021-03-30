0deecfd1a5e24f7807eecf707a53b04013aa2f18
serkanturgay <77546669+serkanturgay@users.noreply.github.com>
2021-03-30 18:55:36 +0300

    fixed

--- go.mod
+++ go.mod
@@ -10,7 +10,7 @@ require (
 	github.com/go-playground/validator/v10 v10.4.1 // indirect
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/golang/protobuf v1.5.1 // indirect
-	github.com/joho/godotenv v1.3.0 // indirect
+	github.com/joho/godotenv v1.3.0
 	github.com/json-iterator/go v1.1.10 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/mattn/go-colorable v0.1.8 // indirect
--- utils/db_util.go
+++ utils/db_util.go
@@ -2,7 +2,7 @@ package util
 
 import (
 	"database/sql"
-	"os"
+
 )
 
 func PanicError(err error) {
@@ -11,7 +11,7 @@ func PanicError(err error) {
 	}
 }
 func DBConn() *sql.DB {
-	connection_string := os.Getenv("DATABASE_URL")
+	connection_string := "doadmin:clhx24ycdanxqvr3@tcp(db-mysql-nyc3-67043-do-user-8989383-0.b.db.ondigitalocean.com:25060)/defaultdb"
 	db, err := sql.Open("mysql", connection_string)
 	PanicError(err)
 
